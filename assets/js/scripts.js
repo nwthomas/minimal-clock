b8673eca2b0ee065be146f2b893e07940306dcbb
“Nathan <nwthomas@me.com>
2018-10-08 12:09:04 -0700

    Clean up CSS and JavaScript code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,14 +5,11 @@ const timeText = document.querySelector(".text");
 var seconds = 0;
 var minutes = 0;
 var hours = 0;
-
 var counter = {
     s: 0,
     m: 0,
     h: 0 
 };
-
-// adds 0 to front of minutes in string
 function getLength(x) {
     if (x.toString().length == 1) {
         return "0" + x.toString();
@@ -20,30 +17,22 @@ function getLength(x) {
         return x;
     }
 }
-
-
-
-// gets text for week day
 function getDate(){
     let dayOfWeek = new Date().getDay();
     return isNaN(dayOfWeek) ? null : ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
         "Friday", "Saturday"][dayOfWeek];
 }
-
-// get text for month
 function getMonth() {
     let monthOfYear = new Date().getMonth();
     return isNaN(monthOfYear) ? null : ["January", "February", "March", "April", "May", "June", 
         "July", "August", "September", "October", "November", "December"][monthOfYear];
 }
-
 function setDate() {
     var previous = {
         s: seconds,
         m: minutes,
         h: hours
     }
-
     const now = new Date();
     const currentDate = now.getDate();
     seconds = now.getSeconds();
@@ -56,21 +45,16 @@ function setDate() {
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     const hoursDegrees = ((hours / 12) * 360) + 90;
     
-    // new code to stop 360 backwards rotation
     if (seconds < previous.s) { counter.s += 1; }
     if (minutes < previous.m) { counter.m += 1; }
     if (hours < previous.h) { counter.h += 1; }
 
-    // text display
     document.querySelector(".text").innerHTML = "Today is " + getDate() + ", " + getMonth() + " " 
         + currentDate + ". The time is " + hours + ":" + getLength(minutes) + ".";
 
-    // rotation of clock hands
     secondHand.style.transform = `rotate(${(secondsDegrees + (360 * counter.s))}deg)`;
     minuteHand.style.transform = `rotate(${(minutesDegrees + (360 * counter.m))}deg)`;
     hourHand.style.transform = `rotate(${(hoursDegrees + (360 * counter.h))}deg)`;
 }
 
 setInterval(setDate, 1000);
-
-
