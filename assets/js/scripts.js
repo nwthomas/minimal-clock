f80356d567d978768d11db0cfbbbd484f9580dd0
Nathan Thomas <nwthomas@me.com>
2025-05-03 10:56:21 -0700

    Hide clock until hand movement starts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+const clock = document.querySelector(".clock");
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".minute-hand");
 const hourHand = document.querySelector(".hour-hand");
@@ -6,55 +7,98 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 var counter = {
-    s: 0,
-    m: 0,
-    h: 0 
+  s: 0,
+  m: 0,
+  h: 0,
 };
 function getLength(x) {
-    if (x.toString().length == 1) {
-        return "0" + x.toString();
-    } else {
-        return x;
-    }
+  if (x.toString().length == 1) {
+    return "0" + x.toString();
+  } else {
+    return x;
+  }
 }
-function getDate(){
-    let dayOfWeek = new Date().getDay();
-    return isNaN(dayOfWeek) ? null : ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
-        "Friday", "Saturday"][dayOfWeek];
+function getDate() {
+  let dayOfWeek = new Date().getDay();
+  return isNaN(dayOfWeek)
+    ? null
+    : [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ][dayOfWeek];
 }
 function getMonth() {
-    let monthOfYear = new Date().getMonth();
-    return isNaN(monthOfYear) ? null : ["January", "February", "March", "April", "May", "June", 
-        "July", "August", "September", "October", "November", "December"][monthOfYear];
+  let monthOfYear = new Date().getMonth();
+  return isNaN(monthOfYear)
+    ? null
+    : [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ][monthOfYear];
 }
 function setDate() {
-    var previous = {
-        s: seconds,
-        m: minutes,
-        h: hours
-    }
-    const now = new Date();
-    const currentDate = now.getDate();
-    seconds = now.getSeconds();
-    minutes = now.getMinutes();
-    hours = now.getHours();
+  var previous = {
+    s: seconds,
+    m: minutes,
+    h: hours,
+  };
+  const now = new Date();
+  const currentDate = now.getDate();
+  seconds = now.getSeconds();
+  minutes = now.getMinutes();
+  hours = now.getHours();
 
-    if (hours > 12) { hours -= 12; }
-    
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    const hoursDegrees = ((hours / 12) * 360) + 90;
-    
-    if (seconds < previous.s) { counter.s += 1; }
-    if (minutes < previous.m) { counter.m += 1; }
-    if (hours < previous.h) { counter.h += 1; }
+  if (hours > 12) {
+    hours -= 12;
+  }
 
-    document.querySelector(".text").innerHTML = "Today is " + getDate() + ", " + getMonth() + " " 
-        + currentDate + ". The time is " + hours + ":" + getLength(minutes) + ".";
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
 
-    secondHand.style.transform = `rotate(${(secondsDegrees + (360 * counter.s))}deg)`;
-    minuteHand.style.transform = `rotate(${(minutesDegrees + (360 * counter.m))}deg)`;
-    hourHand.style.transform = `rotate(${(hoursDegrees + (360 * counter.h))}deg)`;
+  if (seconds < previous.s) {
+    counter.s += 1;
+  }
+  if (minutes < previous.m) {
+    counter.m += 1;
+  }
+  if (hours < previous.h) {
+    counter.h += 1;
+  }
+
+  document.querySelector(".text").innerHTML =
+    "Today is " +
+    getDate() +
+    ", " +
+    getMonth() +
+    " " +
+    currentDate +
+    ". The time is " +
+    hours +
+    ":" +
+    getLength(minutes) +
+    ".";
+
+  secondHand.style.transform = `rotate(${secondsDegrees + 360 * counter.s}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees + 360 * counter.m}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees + 360 * counter.h}deg)`;
+
+  clock.classList.remove("hidden");
 }
 
 setInterval(setDate, 1000);
